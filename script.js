e3ca99b6c870da9a7d1adfb3e44b6a4cb695041e
ISPRiseInGold <altonalibo@gmail.com>
2026-01-08 11:01:24 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,17 +1,18 @@
-const helpBtn = document.getElementById("helpBtn");
-const helpBox = document.getElementById("helpBox");
+// Apparition douce au scroll (style Apple)
+const slides = document.querySelectorAll(".slide");
 
-if (helpBtn) {
-  helpBtn.onclick = () => {
-    helpBox.style.display = helpBox.style.display === "block" ? "none" : "block";
-    helpBox.innerHTML = `
-      <p>Salut, tu te portes bien ?<br><br>
-      Coordination Estudiantine ISP GOMBE.<br>
-      Besoin de nous contacter ?</p>
-      <a href="https://whatsapp.com/channel/0029VbAi4jD2Jl8GG8Odwc06">Chaîne WhatsApp</a><br>
-      <a href="https://chat.whatsapp.com/L0Oyaif4f2T18l2pqCoxhY">Groupe Accueil</a><br>
-      <a href="https://www.facebook.com/profile.php?id=61584401214557">Facebook</a><br>
-      <strong>+243 970 113 745</strong>
-    `;
-  };
-}
+const observer = new IntersectionObserver(entries => {
+  entries.forEach(entry => {
+    if (entry.isIntersecting) {
+      entry.target.style.opacity = "1";
+      entry.target.style.transform = "translateY(0)";
+    }
+  });
+}, { threshold: 0.3 });
+
+slides.forEach(slide => {
+  slide.style.opacity = "0";
+  slide.style.transform = "translateY(40px)";
+  slide.style.transition = "all 1s ease";
+  observer.observe(slide);
+});

a27e1c344bee44684278a1858f31a88c439c9819
ISPRiseInGold <altonalibo@gmail.com>
2026-01-08 11:16:23 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,18 +1,17 @@
-// Apparition douce au scroll (style Apple)
-const slides = document.querySelectorAll(".slide");
+const sections = document.querySelectorAll(".section");
 
 const observer = new IntersectionObserver(entries => {
   entries.forEach(entry => {
     if (entry.isIntersecting) {
-      entry.target.style.opacity = "1";
+      entry.target.style.opacity = 1;
       entry.target.style.transform = "translateY(0)";
     }
   });
 }, { threshold: 0.3 });
 
-slides.forEach(slide => {
-  slide.style.opacity = "0";
-  slide.style.transform = "translateY(40px)";
-  slide.style.transition = "all 1s ease";
-  observer.observe(slide);
+sections.forEach(sec => {
+  sec.style.opacity = 0;
+  sec.style.transform = "translateY(60px)";
+  sec.style.transition = "all 1.2s ease";
+  observer.observe(sec);
 });
